f6fefd7cf5ce3db4bc20c8c8c1bcb3e2c4d5950c
ayyoub134 <ayyoubfassi7@gmail.com>
2023-10-11 11:42:03 -0700

    b

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -5,10 +5,10 @@
  * _putchar - writes the character c to stdout
  * @c: the charcter to print
  *
- * Return: on succes 1.
- *	on error, -1 is returned, and errno is set appropriatrely.
+ * Return: On succes 1.
+ *	On error, -1 is returned, and errno is set appropriately.
  */
-int	_putchar(char c)
+int _putchar(char c)
 {
 	return	(write(1, &c, 1));
 }
